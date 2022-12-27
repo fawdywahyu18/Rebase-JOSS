88da7081aad4e9704af57a1cd8e4fed72209e50b
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2022-12-27 09:44:20 +0700

    Update requirements

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ setup(
     version='0.1.0',
     py_modules=['rebase_os'],
     install_requires=[
-        'numpy==1.23.5',
+        'et-xmlfile==1.1.0',
+        'numpy==1.24.1',
         'openpyxl==3.0.10',
         'pandas==1.5.2',
+        'python-dateutil==2.8.2',
+        'pytz==2022.7',
+        'six==1.16.0',
         ],
     entry_points='''
         [console_scripts]
